79f04005a2987c0b79a7ede58c8090b59081b2be
josh mcgillan <{ID}+{username}@users.noreply.github.com>
2018-09-22 09:48:57 +0930

    crash fix

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var express = require('express')
+var app = express()
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -11,4 +13,13 @@ client.on('message', msg => {
   }
 });
 
-client.login('token');
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname))
+
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
