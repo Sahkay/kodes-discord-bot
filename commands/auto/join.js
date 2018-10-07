fb119d1cd84d46f1fbbcba93f6981c2facb9c54f
josh mcgillan <{username}@users.noreply.github.com>
2018-10-07 12:07:53 +1030

    testing

---
+++ commands/auto/join.js
@@ -0,0 +1,27 @@
+const {
+  Commando
+} = require('discord.js-commando');
+
+module.exports = class JoinCommand extends Commando.Command {
+  constructor(client) {
+    super(client, {
+      name: "join",
+      aliases: ["welcome"],
+      group: "auto",
+      memberName: "join",
+      description: "Sets up the current channel for user join messages.",
+      examples: ["join Welcome USER"],
+      guildOnly: true
+    })
+  }
+
+  run(msg) {
+    const channel = msg.channel;
+    const messageArg = msg.argString;
+    this.client.on("guildMemberAdd", member => {
+      member.guild.channels.get(channel.id).send(messageArg.replace("USER", member), {
+        files: [member.user.avatarURL()]
+      });
+    })
+  }
+}
--- index.js
+++ index.js
@@ -1,46 +1,60 @@
 const axios = require('axios');
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const sqlite = require('sqlite');
+const Commando = require('discord.js-commando');
+const client = new Commando.Client({
+  owner: '206620273443602432',
+  commandPrefix: "!"
+});
+const path = require('path');
 const config = {
   prefix: '!',
   bddatabaseUrl: 'https://bddatabase.net/',
-  searchUrl: 'https://bddatabase.net/ac.php?l=us&term='
+  searchUrl: 'https://bddatabase.net/ac.php?l=us&term=',
+  questRewardUrl: 'https://bddatabase.net/query.php?l=us&a=quests&type=questrewards&id='
 };
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+client.registry.registerGroups([
+  ['auto', 'Automatic commands such as join/leave messages.'],
+  ['roles', 'Commands related to roles and their assignment.'],
+  ['moderation', 'Commands related to server moderation']
+]).registerDefaults().registerCommandsIn(path.join(__dirname, 'commands'))
 
-client.on('message', msg => {
-  if (msg.author.bot) return;
-  if (msg.content.indexOf(config.prefix) !== 0) return;
-  const args = msg.content.slice(config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-  if (command === 'ping') {
-    msg.reply('pong');
-  } else if (command === 'bdo' && args.length > 0) {
-    let type = args[0];
-    if (args[0] === "search" || args[0] === "s") {
-      if (args.length < 2) {
-        msg.reply("Missing item name argument.")
-      } else {
-        axios.get(config.searchUrl + args[1]).then(response => {
-          console.log(response.data);
-          console.log(typeof response.data);
-          let results = JSON.parse(response.data.trim());
-          let message = "If your desired item is not below please refine your search:\n";
-          console.log(results);
-          results = results.slice(0, 10);
-          results.forEach(function(value, index) {
-            message += value.name + " " + value.value + "\n";
-          })
-          msg.channel.send(message);
-        }).catch(error => {
-          console.log(error);
-        })
-      }
-    }
-  }
-});
+client
+  .on('error', console.error)
+  .on('warn', console.warn)
+  .on('debug', console.log)
+  .on('ready', () => {
+    console.log(`Client ready; logged in as ${client.user.username}#${client.user.discriminator} (${client.user.id})`);
+  })
+  .on('disconnect', () => {
+    console.warn('Disconnected!');
+  })
+  .on('reconnecting', () => {
+    console.warn('Reconnecting...');
+  })
+  .on('commandError', (cmd, err) => {
+    if (err instanceof commando.FriendlyError) return;
+    console.error(`Error in command ${cmd.groupID}:${cmd.memberName}`, err);
+  })
+  .on('commandBlocked', (msg, reason) => {
+    console.log(oneLine `
+			Command ${msg.command ? `${msg.command.groupID}:${msg.command.memberName}` : ''}
+			blocked; ${reason}
+		`);
+  })
+  .on('commandStatusChange', (guild, command, enabled) => {
+    console.log(oneLine `
+			Command ${command.groupID}:${command.memberName}
+			${enabled ? 'enabled' : 'disabled'}
+			${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
+		`);
+  })
+  .on('groupStatusChange', (guild, group, enabled) => {
+    console.log(oneLine `
+			Group ${group.id}
+			${enabled ? 'enabled' : 'disabled'}
+			${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
+		`);
+  });
 
 client.login(process.env.TOKEN)
